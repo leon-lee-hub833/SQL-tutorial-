fd4b5e439c7389a217f38abacdaf1d83c8b75693
dayanglee <li412701180@gmail.com>
2025-04-28 15:31:51 +0700

    update aggregate functions

--- 04_01_Aggregate Functions.sql
+++ 04_01_Aggregate Functions.sql
@@ -1,20 +1,26 @@
 /* 
-分组函数 又叫 统计函数， 聚合函数 
-1分类：
-sum() 求和， avg() 平均值， max() 最大值， min（）最小值， count() 计数 
+aggregate function
+1. Categories 
+sum() 
+ avg() 
+ max() 
+ min（） 
+ count()  
 
-2特点:
-1 sum. avg 一般处理数值类型， max, min, count() 可以处理任何类型 
-2 所有分类都忽略了null 值 
+2 Features :
+1 sum. avg generally handle numeric types, 
+  max, min, count() can handle any data type 
+  
+2 all aggregate functions automatically ignore Null values  
 
-3 用关键字 distinct 去重
+3 Use the keyword Distinct to remove duplicates
 
-4 count() 函数的介绍 
+4 count() 
 
-5 和分组函数查询的字段 只能是group by 后的字段
+5 when querying with aggregate functions, you can only select fields that appear after group by
 */
 
-# 1.1简单单个运算 
+# 1.1 basic caculation 
 select sum(salary) from employees;
 select avg(salary) from employees;
 select max(salary) from employees;
@@ -22,39 +28,42 @@ select max(salary) from employees;
 select min(salary) from employees;
 select count(salary)from employees;
 
-# 1.2多个一起查询 
+# 1.2 mutipule query caculation at once 
 select sum(salary), avg(salary),max(salary),min(salary),count(salary)
 from employees;
 
-# 1.3函数可嵌套 
+# 1.3 functions can be nested
 select sum(salary), round(avg(salary), 2) ,max(salary),min(salary),count(salary)
 from employees;
 
-# 2.1 sum. avg 一般处理数值类型， max, min, count() 可以处理任何类型 
-select sum(last_name), avg(last_name) from employees; # 显示0 
-select max(last_name) from employees; #显示 Zlotkey 按字母排序  
-select min(last_name) from employees; #显示 Abel 按字母排序  
+# 2.1 sum. avg generally deal with numerica typle，while max, min, count() can deal with any types 
+select sum(last_name), avg(last_name) from employees; #  result 0 ( cause can not deal with text)
+select max(last_name) from employees; # result Zlotkey order by A-Z  
+select min(last_name) from employees; # result  Abel order by A-Z
 select count(last_name) from employees; # 107
 
-# 2.2 所有分类都忽略了null 值 
+# 2.2 all numerical fuctions ignore null value 
 select sum(commission_pct), avg(commission_pct) from employees;
 select avg(commission_pct), sum(commission_pct)/35, sum(commission_pct)/107 from employees;
 select min(commission_pct), max(commission_pct) from employees;
 select count(commission_pct) from employees;
 
-# 3. 用关键字 distinct 去重 不加逗号
+# 3. use distinct remove duplicates
 select sum(distinct salary) , sum(salary) from employees;
 select count(distinct salary), count(salary) from employees;
-# 用的最多的这两个  
 
-# 4. count() 函数的介绍 有四种方法可以计数  
-select count(salary) from employees;  # 单个行的查询
-select count(*) from employees; # 全部查询
-select count(1) from employees; # 全部查询
-select count('haha') from employees; # 全部查询 不推荐  
+# 4. count() function there are 4 types as below 
+use myemployees;
+select count(salary) from employees;  # query with single column
+select count(*) from employees; # query all columns 
+select count(1) from employees; # query all columns 
+select count('haha') from employees; # query all columns not recommend   
 
-# 5.和分组函数查询的字段 只能是group by 后的字段
-# 和分组函数查询的字段， 表格需要一致 
+# 5.If a SELECT statement contains both aggregation functions and normal fields, the normal fields must be included in the GROUP BY clause.
 select count(salary), salary from employees; # 报错 
+
+select sum(salary), salary 
+from employees 
+group by salary;
 
 
