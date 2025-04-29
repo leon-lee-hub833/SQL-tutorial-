0d773619375fb3e9bff56b4c36e8cc117bc2b944
dayanglee <li412701180@gmail.com>
2025-04-29 21:09:33 +0700

    update query with join

--- 06_query_with_join.sql
+++ 06_query_with_join.sql
@@ -1,38 +1,41 @@
 /*
-sql99 支持
-内连接 ： 等值连接， 非等值连接， 自连接 
-外连接： 左外连接， 右外连接， 全连接（ mysql不支持） 
-交叉连接 
+type of join query 
+inner join ： equijoins， non-equijoins， self-joins 
+outer join： left outer join， right outer join， full outer join（ mysql does not suppport full outer join） 
+cross join : cartesian product of two table 
 
-语法：
-内连接 
+syntax：
+inner join:
+ 
 select ...
 from ...
 (inner) join ...... on .....=... 
 
-特点：
-1. 可添加，筛选， 排序， 分组 
-2. 可以省略 inner 
+Features：
+1. can add filters， sorting，grouping etc.
+2. The key word inner can be omitted
 
-外连接 
+
+syntax:
+cross join
+
 select....
 from ....
-left/right (outor) join .... on .....= ...
+cross join .... 
 
-交叉连接 
-select....
-from ....
-cross join .... on .....= ...
+cross join = Cartesian product, if Table 1 has m rows and Table 2 has n rows, the resulting table will have m × n rows.
+
 */
 
-#1 内连接 
-#1.1 等值内连接  
-# 案例： 查询员工名， 部门名
+#1 inner join
+#1.1 inner join < equijoins>
+# query last_name  and department name 
 select last_name, department_name 
 from  employees as e 
-join departments as d on e.department_id = d.department_id;  # 两个表可以调换顺序 
+join departments as d on e.department_id = d.department_id;  # The two tables can be switched.
 
-#案例：查询工种名和名字中包含E的员工名 （ 包含查询条件: 模糊查询） 
+
+# Query the job ID and the names of employees whose names contain the letter "E". （ include where conditions: like） 
 select last_name, job_title
 from employees as e
 join jobs as j
@@ -40,7 +43,7 @@ on e.job_id = j.job_id
 where last_name like'%e%';
 
 
-#案例3， 查询部门分组>3的城市名， 和部门个数， （分组+筛选） 
+# Query the city names and the number of departments in each city, where the number of departments is greater than 3 (grouping + filtering). 
 select city, count(*) 
 from locations as l 
 join departments as d 
@@ -48,7 +51,7 @@ on l.location_id = d.location_id
 group by  city
 having count(*) >3;
 
-# 案例4： 查询哪个部门的部门员工个数>3的部门名和员工个数， 按个数降序 
+# Query the department names and the number of employees for departments that have more than 3 employees, sorted in descending order by the number of employees.
 select department_name , count(*) 
 from departments as d 
 join employees as e 
@@ -57,15 +60,15 @@ group by department_name
 having count(*) >3
 order by count(*) desc;
 
-# 案例5： 查询员工名， 部门名，工种名 ，按照部门名降序 （三表连接+排序） 
-select e.last_name, d.department_name, j.job_title  # ( 要加库名.表名） 
+#  Query the employee names, department names, and job titles, sorted in descending order by department name (join three tables and sort).
+select e.last_name, d.department_name, j.job_title  
 from employees as e 
 join departments as d on e.department_id = d.department_id
 join jobs as j on e.job_id = j.job_id
 order by department_name desc;
 
-# 1.2 非等值连接 
-# 员工的工资级别 
+# 1.2 inner join<non-equijoins>
+# Query salary grade of employees.
 select salary, grade_level
 from employees 
 join job_grades 
@@ -73,7 +76,7 @@ on salary BETWEEN lowest_sal and highest_sal;
 
 select * from job_grades;
 
-# 查询工资级别的个数>2， 并且按工资级别降序  ( 大于20的工资级别有多少）
+# Query the salary grades that have more than 20 employees, and sort them in descending order by salary grade.
 select grade_level, count(*)
 from employees as e
 join job_grades as j
@@ -82,28 +85,58 @@ group by grade_level
 having count(*) > 20
 order by grade_level desc;
 
-#3 自连接 
-# 查询员工的名字， 上级的名字 ( 我怎么知道哪个是领导， 哪个是员工呢？）  
-select e.last_name as 员工名字, m.last_name as 领导名字
+#3 inner join < self-join>
+# Query employee's name and their manager's name  
+select e.last_name as employees_name, m.last_name as manager_name
 from employees as e
 join employees as m
 on m.manager_id = e.employee_id ;
 
-select * from employees;
-#  查询姓名中包含字符K的 员工的名字， 上级的名字 ( 我怎么知道哪个是领导， 哪个是员工呢？）  
-select e.last_name as 员工名字, m.last_name as 领导名字
+#  Query the names of employees whose names contain the letter 'K', and the names of their managers. 
+select e.last_name as employees_name, m.last_name as manager_name
 from employees as e
 join employees as m
 on m.manager_id = e.employee_id 
 where e.last_name like '%k%';
 
-# 外连接  
-# 左外连接  A ，B两个表 用A表的目标列的全部匹配B的目标行 没有的 null 
+
+
+
+
+# outer join  
+/*
+outer join divide to 
+
+left outer join 
+example 
+left table have name column 
+right table have department column 
+name + IT -match - show both rows 
+name + null - department not found 
+wont appear - name not found 
+
+right outer join 
+left table have name column 
+right table have department column 
+name + IT -match - show both rows 
+null + IT- name not found
+wont appear - department not found
+
+full outer join
+musql not support fll outer join 
+
+syntax: 
+selet ....
+from tabel 2
+left/right [outer] join table2 on key1 = key 2 
+
+*/
 
 select * from girls.beauty;
 select * from girls.boys;
 
-# 案例：查询没有男朋友的女人的名字
+# outer join <left join>
+# query the girl's name who haven.t boyfriend 
 select name, boyName 
 from beauty 
 left join boys 
@@ -115,26 +148,28 @@ from boys
 left join beauty
 on beauty.boyfriend_id= boys.id;
 
-
-# 右外连接 
-select name, boyName 
-from boys
-right join beauty
-on beauty.boyfriend_id= boys.id
-where boyName is null;
-
-
-# 案例 哪个部门没有员工
+# query which department haven't employee 
 select department_name, e.employee_id
 from departments as d 
 left join employees as e 
 on d.department_id = e.department_id
 where e.employee_id is null;
 
-# 全外连接， mysql 不支持 
-# 全外查询的是交集部门，主表有从表没有的null , 从表有主表没有的 null  
 
-# 交叉连接 = 卡迪尔乘急， 每一行链接  
+# outer join < right join>
+select name, boyName 
+from boys
+right join beauty
+on beauty.boyfriend_id= boys.id
+where boyName is null;
+
+# full outer join
+# mysql not support fll outer join 
+
+/*
+cross join = Cartesian product, if Table 1 has m rows and Table 2 has n rows, the resulting table will have m × n rows.
+
+*/ 
 use girls;
 select b.*, bo.* 
 from beauty as b 
